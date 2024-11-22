3ca4391f80afd46167f85fce979ad0f130bf71bb
Milan Fox <31571437+MilanFox@users.noreply.github.com>
2024-11-22 17:25:57 +0100

    feat(ui): block out basic ui, add logo, add icons
    add logo, add icon library, add basic landmarks for GameView, add placeholders for avatar, menu and logo

---
+++ src/components/molecules/Landmarks/Header.vue
@@ -0,0 +1,45 @@
+<template>
+  <header class="header">
+    <Avatar class="header__avatar"/>
+    <h1 class="header__headline"><img src="/logo.png" alt="By Royal Decree" class="header__logo"/></h1>
+    <MenuIcon :size="40" class="header__menu"/>
+  </header>
+</template>
+
+<script setup lang="ts">
+import Avatar from '@atoms/Avatar/Avatar.vue';
+import MenuIcon from 'vue3-material-design-icons-ts/dist/Menu.vue';
+</script>
+
+<style lang="scss">
+.header {
+  height: 100px;
+  display: flex;
+  align-items: end;
+  justify-content: space-between;
+
+  &__headline {
+    height: 80%;
+  }
+
+  &__logo {
+    height: 100%;
+    width: 0;
+
+    @include from-breakpoint(tablet) {
+      width: unset;
+    }
+  }
+
+  /* PLACEHOLDER COMPONENT */
+  &__avatar {
+    height: 80%;
+
+  }
+
+  /* PLACEHOLDER COMPONENT */
+  &__menu {
+    align-self: center;
+  }
+}
+</style>
---
+++ src/components/molecules/Panes/IDE.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="game-canvas">
+    <div class="pane__title">
+      <CodeIcon title=""/>
+      <h2>Code</h2>
+    </div>
+    <div class="pane__body">
+
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import CodeIcon from 'vue3-material-design-icons-ts/dist/FileCodeOutline.vue';
+</script>
