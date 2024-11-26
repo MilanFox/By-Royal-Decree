0ab54396466843e536edf0906de872f442951970
Milan Fox <milan.fox@linuxmail.org>
2024-11-26 18:34:04 +0100

    feat(ui): add user-settings store

---
+++ src/stores/userConfig.ts
@@ -0,0 +1,10 @@
+import { defineStore } from 'pinia';
+import { ref } from 'vue';
+
+export const useUserConfigStore = defineStore('userConfig', () => {
+  const shouldRenderGrid = ref(true);
+  const shouldAnimateSprites = ref(true);
+
+  return { shouldRenderGrid, shouldAnimateSprites };
+},
+);
