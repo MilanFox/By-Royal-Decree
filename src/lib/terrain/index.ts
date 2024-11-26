7892efc84beb11917340f4f85f6062611b67fb33
Milan Fox <milan.fox@linuxmail.org>
2024-11-26 18:41:05 +0100

    chore(renderer): simplify map blueprint language

--- src/lib/terrain/index.ts
+++ src/lib/terrain/index.ts
@@ -1,24 +1,18 @@
 import { Tile } from './tile.class';
 
 export const tileFactory = {
-  _: () => null,
-  grass: (x: number, y: number) => new Tile({
+  '🟦': () => null,
+  '🟩': (x: number, y: number) => new Tile({
     name: 'grass',
     tiling: '4x4',
     spriteSource: '/sprites/terrain/grass.png',
     x,
     y,
   }),
-  sand: (x: number, y: number) => new Tile({
+  '🟨': (x: number, y: number) => new Tile({
     name: 'sand',
     tiling: '4x4',
     spriteSource: '/sprites/terrain/sand.png',
-    x,
-    y,
-  }),
-  water: (x: number, y: number) => new Tile({
-    name: 'water',
-    spriteSource: '/sprites/terrain/water.png',
     x,
     y,
   }),
