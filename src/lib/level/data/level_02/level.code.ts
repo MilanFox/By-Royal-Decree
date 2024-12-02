f43e4a03c4e125e91eda957f9429d5e4325a6f60
Milan Fox <milan.fox@linuxmail.org>
2024-12-02 11:27:24 +0100

    feat(level): add demo level 2

---
+++ src/lib/level/data/level_02/level.code.ts
@@ -0,0 +1,22 @@
+const pawn =
+  `// Order your entities to do your bidding - your word is law.
+// currently only "walk()" and "endRoutine()" have been implemented
+
+// Example:
+await entity.walk('right');
+await entity.walk('down');
+await entity.walk('right');
+await entity.walk('up');
+
+entity.endRoutine();
+
+// Unreachable because routine has ended already:
+await entity.walk('up');`;
+
+const knight =
+  `//You'll figure it out from here...
+
+await entity.walk('right');
+entity.endRoutine();`;
+
+export const defaultCode = { pawn, knight };
--- src/lib/level/index.ts
+++ src/lib/level/index.ts
@@ -1,6 +1,7 @@
 import level_01 from './data/level_01';
+import level_02 from './data/level_02';
 
-export default [level_01];
+export default [level_01, level_02];
 
 export * from './level.class';
 export * from './level.types';
