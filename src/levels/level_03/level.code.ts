bc71fbe59ab07934b5a9c5afc531c3fe39428134
Milan Fox <milan.fox@linuxmail.org>
2024-12-04 00:29:16 +0100

    feat(game-logic): add default code to demo level 3

--- src/levels/level_03/level.code.ts
+++ src/levels/level_03/level.code.ts
@@ -1,3 +1,20 @@
-const pawn = '';
+const pawn =
+  `while (true) {
+    await pawn.walk('right');
+    const curPos = pawn.feel();
+
+    if (curPos.resource && curPos.resource.name === 'gold') {
+        pawn.pickUp();
+        await pawn.walk('right');
+        await pawn.walk('down');
+        await pawn.walk('down');
+        await pawn.walk('left');
+        pawn.drop();
+        break;
+    }
+}
+
+await pawn.walk('left');
+pawn.endRoutine();`;
 
 export const defaultCode = { pawn };
