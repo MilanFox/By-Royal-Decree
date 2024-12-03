49696d22f2f0df341b817481bc034ed77f96eee1
Milan Fox <milan.fox@linuxmail.org>
2024-12-04 00:17:18 +0100

    feat(game-logic): change "feel()" so it gives individual entity info

--- src/levels/level_03/level.entities.ts
+++ src/levels/level_03/level.entities.ts
@@ -1,9 +1,11 @@
 import type { LevelEntityBluePrint } from '@/levels';
 import { entityColor } from '@lib/entities/_base';
 
+const randomTree = [2, 4, 6, 8, 10][Math.floor(Math.random() * 5)];
+
 export const entities: LevelEntityBluePrint = {
   pawns: [
-    { x: 1, y: 1, color: entityColor.RED },
+    { x: 1, y: 2, color: entityColor.RED },
   ],
   trees: [
     { x: 2, y: 3 },
@@ -16,6 +18,6 @@ export const entities: LevelEntityBluePrint = {
     { x: 4, y: 7, name: 'gold' },
     { x: 6, y: 7, name: 'meat' },
     { x: 8, y: 7, name: 'wood' },
-    { x: 2, y: 1, name: 'gold' },
+    { x: randomTree, y: 2, name: 'gold' },
   ],
 };
--- src/lib/entities/_base/movableEntity/movableEntity.types.ts
+++ src/lib/entities/_base/movableEntity/movableEntity.types.ts
@@ -1,11 +1,18 @@
-import { Entity, type EntityProps } from '@lib/entities/_base/';
+import { type EntityProps } from '@lib/entities/_base/';
 import type { Tile } from '@lib/terrain/tile.class';
+import { Resource } from '@lib/entities/resource';
+import { Pawn } from '@lib/entities/pawn';
+import { Knight } from '@lib/entities/knight';
+import { Tree } from '@lib/entities/tree';
 
 export interface MovableEntityProps extends EntityProps {
   speed: number;
 }
 
 export interface FeelData {
-  entity?: Entity;
+  pawn?: Pawn;
+  knight?: Knight;
+  tree?: Tree;
+  resource?: Resource;
   landTile?: Tile;
 }
