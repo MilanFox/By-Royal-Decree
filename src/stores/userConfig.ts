7113a9327d565680eca9f4d38d5d2df07d242435
Milan Fox <milan.fox@linuxmail.org>
2024-12-04 14:45:38 +0100

    feat(ui): add ui for changing the game speed

--- src/stores/userConfig.ts
+++ src/stores/userConfig.ts
@@ -4,6 +4,7 @@ import { useLocalStorage } from '@vueuse/core';
 export const useUserConfigStore = defineStore('userConfig', () => {
   const shouldRenderGrid = useLocalStorage('userSetting.shouldRenderGrid', false);
   const shouldAnimateSprites = useLocalStorage('userSetting.shouldAnimateSprites', true);
+  const gameSpeed = useLocalStorage('userSetting.gameSpeed', 2);
 
-  return { shouldRenderGrid, shouldAnimateSprites };
+  return { shouldRenderGrid, shouldAnimateSprites, gameSpeed };
 });
